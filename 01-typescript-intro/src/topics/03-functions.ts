192dc143b5ca8f998e4e9f95e37d305bbcf81ba6
devAlbertodc <aldacala@hotmail.es>
2023-04-25 22:04:43 +0200

    Basics of typescript, use interface and objects

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -14,10 +14,36 @@ function multiply (firstNumber:number, secondNumber?:number, base=2){
 }
 
 //Assign the returned value of the function to a variable:
-const result:number = addNumbers(1,2);
+/*const result:number = addNumbers(1,2);
 const result2:string = addNumbersArrow(1,2)
-
 const multiplyResult:number = multiply(5);
 console.log({result, result2, multiplyResult})
+*/
+
+//Properties for object:
+interface Character{
+    name: string,
+    hp: number,
+    showHp: () => void
+}
+
+//Character datatype makes strict, can't pass another type of data.
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+    console.log(character);
+}
+
+//Generate a character:
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp(){
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+//Testing the interfaces and functions:
+healCharacter(strider,10);
+strider.showHp();
 
 export{};
