41ddd1eb80995162487b301c64094fd655a6a169
devAlbertodc <aldacala@hotmail.es>
2023-04-26 22:21:00 +0200

    Agregar homework de interfaces

---
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -0,0 +1,31 @@
+//When an interface has an object, we can create another interface to simplify and make the code easier to read:
+interface SuperHero {
+	name: string;
+	age: number;
+	address: Address;
+	showAddress: () => string;
+}
+
+interface Address {
+	street: string;
+	country: string;
+	city: string;
+}
+
+const superHeroe: SuperHero = {
+	name: "Spiderman",
+	age: 30,
+	address: {
+		street: "Main St",
+		country: "USA",
+		city: "NY",
+	},
+	showAddress() {
+		return this.name + ", " + this.address.city + ", " + this.address.country;
+	},
+};
+
+const address = superHeroe.showAddress();
+console.log(address);
+
+export {};
