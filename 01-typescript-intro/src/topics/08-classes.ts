1b308e921aa7bf0dca9b9522377053f1505e5e4f
devAlbertodc <aldacala@hotmail.es>
2023-04-27 23:11:55 +0200

    Inherit classes in typescript

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -13,5 +13,17 @@ class Person {
     };
 }
 
-const ironman = new Person("Ironman", "New York");
-console.log(ironman.address);
+//This class will inherate all the properties from the parent class:
+export class Hero extends Person {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+    ) {
+        //Call the parent constructor sending name and address
+        super(realName, 'New York');
+    }
+}
+
+const ironman = new Hero("Ironman", 45, "Tony");
+console.log(ironman);
