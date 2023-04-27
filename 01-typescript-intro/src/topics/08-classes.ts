bff5e0ba2d3315162ba66c41a045eae0f7450bba
devAlbertodc <aldacala@hotmail.es>
2023-04-27 22:44:48 +0200

    Adding short-hand way to create the constructor

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,15 +1,17 @@
 class Person {
     //Properties of the class:
-    public name: string;
+    //public name: string;
     //Private properties are only accesible from the class:
-    private address: string;
+    //private address: string;
 
-    //Methods of the class:
-    constructor() {
-        this.name = "Fernando";
-        this.address = "New York";
+    //First method to be called when an object of the class is created:
+    constructor(
+        public name: string,
+        private address: string = 'No address') {
+        //this.name = name;
+        //this.address = address;
     };
 }
 
-const ironman = new Person();
+const ironman = new Person("Ironman", "New York");
 console.log(ironman.address);
