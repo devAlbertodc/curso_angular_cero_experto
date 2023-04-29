573bcdb4b37002ca775d536266ce157dab83d638
devAlbertodc <aldacala@hotmail.es>
2023-04-29 23:50:00 +0200

    Add decorators to keep same code in class. Decorators change behaviour of classes, methods...

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -8,7 +8,8 @@ import "./style.css";
 //import "./topics/06-function-destructuring.ts";
 //import "./topics/07-import-export.ts"
 //import "./topics/08-classes"
-import "./topics/09-generics"
+//import "./topics/09-generics"
+import "./topics/10-decorators"
 
 // rome-ignore lint/style/noNonNullAssertion: <explanation>
 const app = document.querySelector<HTMLDivElement>('#app')!
