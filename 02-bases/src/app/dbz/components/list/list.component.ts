44ac0b9c0228d0236732159a0de1982729bcfe0c
devAlbertodc <aldacala@hotmail.es>
2024-01-22 21:39:00 +0100

    Manage @Input() to pass properties to child

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  //If dbz-selector does not receive the property, this will be the default content to render
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
 }
