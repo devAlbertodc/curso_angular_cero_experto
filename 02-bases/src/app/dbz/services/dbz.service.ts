9d96072a23361f92a92779c9424fbcc782563719
devAlbertodc <aldacala@hotmail.es>
2024-02-07 20:28:04 +0100

    Implement unique id to not use indexes

--- 02-bases/src/app/dbz/services/dbz.service.ts
+++ 02-bases/src/app/dbz/services/dbz.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid'
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -6,21 +7,25 @@ export class DbzService {
 
   public characters: Character[] = [
     {
+      id: uuid(),
       name: 'Krillin',
       power: 1000
     },
     {
+      id: uuid(),
       name: 'Goku',
       power: 9500
     },
     {
+      id: uuid(),
       name: 'Vegeta',
       power: 7500
     }
   ]
 
   onNewCharacter(character: Character): void {
-    this.characters.push(character);
+    const newCharacter: Character = { id: uuid(), ...character }
+    this.characters.push(newCharacter);
   }
 
   onDeleteCharacter(index: number) {
